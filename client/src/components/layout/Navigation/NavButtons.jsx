f27f983c0926ecfdb01a170a48ecacf79439118e
shankaravi6 <shankaravi6.india@gmail.com>
2024-07-09 00:04:17 +0530

    Set to admin login page redirect in navigation login, but future it will be changed to user login

--- client/src/components/layout/Navigation/NavButtons.jsx
+++ client/src/components/layout/Navigation/NavButtons.jsx
@@ -8,6 +8,8 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import { useDispatch, useSelector } from 'react-redux';
 import { setMode } from '../../../store';
 import { colorTokens } from '../../../context/theme/theme';
+import { Link } from "react-router-dom";
+
 
 const NavButtons = () => {
 
@@ -16,7 +18,7 @@ const NavButtons = () => {
 
   return (
     <SoFlex gap='20px'>
-        <SoPlainButton>LOGIN</SoPlainButton>
+        <SoPlainButton><Link to='/admin'>LOGIN</Link></SoPlainButton>
         {mode == 'light' ? <SoCover className='cursor-pointer' onClick={() => {dispatch(setMode("dark"))}}><DarkModeIcon/></SoCover> : <SoCover className='cursor-pointer' onClick={() => {dispatch(setMode("light"))}}><LightModeIcon style={{color:`${colorTokens.drops[200]}`}} /></SoCover>}
     </SoFlex>
   );
